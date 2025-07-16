e5a9bd12d9d78f96629fab0d8bdeefeb818c76bd
Saad Abbas <2023cs598@student.uet.edu.pk>
2025-07-16 14:26:08 +0500

    update

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -13,7 +13,7 @@ import Intro from "./pages/Intro";
 import ProfessionalCertificates from "./pages/Procertificates";
 import EmailIcon from "./pages/Email";
 import Education from "./pages/Education";
-import { ThemeProvider } from "./context/colorTheme";
+import { ThemeProvider } from "./context/ColorTheme";
 
 function App() {
   const [loadingDone, setLoadingDone] = useState(false);
